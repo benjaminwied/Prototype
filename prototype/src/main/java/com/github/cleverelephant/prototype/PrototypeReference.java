22eb76d6296d6150fa775a2e48a3776dc91fc4c7
CleverElephant <88872078+CleverElephant@users.noreply.github.com>
2022-12-29 15:38:43 +0100

    Add prototype references
    resolves #2
    
    Signed-off-by: CleverElephant <88872078+CleverElephant@users.noreply.github.com>

---
+++ prototype/src/main/java/com/github/cleverelephant/prototype/PrototypeReference.java
@@ -0,0 +1,150 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2022 Benjamin Wied
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.cleverelephant.prototype;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.fasterxml.jackson.annotation.OptBoolean;
+
+/**
+ * A reference to a prototype, usually used from within a prototype definition / builder.
+ *
+ * @author     Benjamin Wied
+ *
+ * @param  <T>
+ *             type
+ * @param  <P>
+ *             prototype
+ */
+public class PrototypeReference<T, P extends Prototype<T>>
+{
+    private static final Pattern RELATIVE_PARENT_REMOVE = Pattern
+            .compile("/\\w+/\\.\\.", Pattern.UNICODE_CHARACTER_CLASS);
+
+    private final String targetPrototypeName;
+
+    /**
+     * Constructs a new {@code PrototypeReference} using the given {@code sourcePrototypeName} and
+     * {@code relativeTargetName}.<br>
+     * <br>
+     * Similar to {@link Class#getResource(String)}, if {@code relativeTargetName} starts with '/', it is considered the
+     * absolute prototype name. If not, the absolute prototype name is constructed using the folder of
+     * {@code sourcePrototypeName}.
+     *
+     * @param  sourcePrototypeName
+     *                                  name of the prototype this reference originates from
+     * @param  relativeTargetName
+     *                                  relative or absolute path to target prototype
+     *
+     * @throws NullPointerException
+     *                                  if either {@code sourcePrototypeName} or {@code relativeTargetName} is null
+     * @throws IllegalArgumentException
+     *                                  if either {@code sourcePrototypeName} or the absolute path to the target
+     *                                  prototype is not a valid prototype name (as defined by
+     *                                  {@link PrototypeManager#checkName(String)}.
+     */
+    public PrototypeReference(
+            @JacksonInject(useInput = OptBoolean.FALSE, value = "name") String sourcePrototypeName,
+            String relativeTargetName
+    )
+    {
+        Objects.requireNonNull(sourcePrototypeName, "sourcePrototypeName must not be null");
+        Objects.requireNonNull(relativeTargetName, "relativeTargetName must not be null");
+        PrototypeManager.checkName(sourcePrototypeName);
+
+        if (relativeTargetName.startsWith("/"))
+            targetPrototypeName = compressPath(relativeTargetName.substring(1));
+        else {
+            String prototypePackage;
+            if (sourcePrototypeName.contains("/"))
+                prototypePackage = sourcePrototypeName.substring(0, sourcePrototypeName.lastIndexOf("/")) + "/";
+            else
+                prototypePackage = "";
+
+            targetPrototypeName = compressPath(prototypePackage + relativeTargetName);
+        }
+
+        PrototypeManager.checkName(targetPrototypeName);
+    }
+
+    private static String compressPath(String path)
+    {
+        while (path.contains("/./"))
+            path = path.replace("/./", "/");
+
+        return RELATIVE_PARENT_REMOVE.matcher(path).replaceAll("");
+    }
+
+    /**
+     * Builds a type of the prototype this reference points to.
+     *
+     * @return                          the type built
+     *
+     * @throws IllegalArgumentException
+     *                                  if the prototype this reference points to does not exist
+     */
+    public T createType()
+    {
+        return PrototypeManager.createType(targetPrototypeName);
+    }
+
+    /**
+     * Returns an Optional containing the prototype this reference points to, or an empty Optional if no prototype could
+     * be found. This is a delegation method to {@link PrototypeManager#getPrototype(String)}.
+     *
+     * @return the prototype this reference points to
+     *
+     * @see    PrototypeManager#getPrototype(String)
+     * @see    #getPrototype()
+     */
+    public Optional<P> getOptionalPrototype()
+    {
+        return PrototypeManager.<T, P>getPrototype(targetPrototypeName);
+    }
+
+    /**
+     * Returns the prototype this reference points to, or throws an {@link PrototypeException} if not prototype could be
+     * found.
+     *
+     * @return the prototype this reference points to
+     *
+     * @see    #getOptionalPrototype()
+     */
+    public P getPrototype()
+    {
+        return getOptionalPrototype()
+                .orElseThrow(() -> new PrototypeException("no prototype found with name " + targetPrototypeName));
+    }
+
+    /**
+     * @return the name of the prototype this reference points to
+     */
+    public String getTargetPrototypeName()
+    {
+        return targetPrototypeName;
+    }
+}
---
+++ prototype/src/test/java/com/github/cleverelephant/prototypetest/PrototypeReferenceTest.java
@@ -0,0 +1,99 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2022 Benjamin Wied
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.cleverelephant.prototypetest;
+
+import com.github.cleverelephant.prototype.PrototypeReference;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.InjectableValues;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SuppressWarnings("javadoc")
+class PrototypeReferenceTest
+{
+    static ObjectMapper OBJECT_MAPPER;
+
+    @BeforeAll
+    static void setUpBeforeClass()
+    {
+        OBJECT_MAPPER = new ObjectMapper();
+    }
+
+    @AfterAll
+    static void tearDownAfterClass()
+    {
+        OBJECT_MAPPER = null;
+    }
+
+    @Test
+    void testAbsolutePath()
+    {
+        PrototypeReference<?, ?> reference = new PrototypeReference<>("abc", "/path/to/my/proto");
+        assertEquals("path/to/my/proto", reference.getTargetPrototypeName(), "wrong absolte path");
+    }
+
+    @Test
+    void testRelativePath()
+    {
+        PrototypeReference<?, ?> reference = new PrototypeReference<>("abc", "path/to/my/proto");
+        assertEquals("path/to/my/proto", reference.getTargetPrototypeName(), "wrong relative path");
+
+        reference = new PrototypeReference<>("folder/abc", "path/to/my/proto");
+        assertEquals("folder/path/to/my/proto", reference.getTargetPrototypeName(), "wrong relative path");
+
+        reference = new PrototypeReference<>("folder/abc", "proto");
+        assertEquals("folder/proto", reference.getTargetPrototypeName(), "wrong relative path");
+    }
+
+    @Test
+    void testPathCompression()
+    {
+        PrototypeReference<?, ?> reference = new PrototypeReference<>("abc", "path/./to/.my/././proto");
+        assertEquals("path/to/.my/proto", reference.getTargetPrototypeName(), "wrong compressed path");
+
+        reference = new PrototypeReference<>("abc", "path/to/my/other/../proto");
+        assertEquals("path/to/my/proto", reference.getTargetPrototypeName(), "wrong compressed path");
+
+        reference = new PrototypeReference<>("abc", "path/to/my/other/./../proto");
+        assertEquals("path/to/my/proto", reference.getTargetPrototypeName(), "wrong compressed path");
+    }
+
+    @Test
+    void testDeserialization() throws JsonMappingException, JsonProcessingException
+    {
+        String json = "\"path/to/my/proto\"";
+        InjectableValues injectableValues = new InjectableValues.Std().addValue("name", "folder/abc");
+        PrototypeReference<?,
+                ?> reference = OBJECT_MAPPER.reader(injectableValues).forType(PrototypeReference.class).readValue(json);
+        assertEquals("folder/path/to/my/proto", reference.getTargetPrototypeName(), "deserialization failed");
+    }
+
+}
