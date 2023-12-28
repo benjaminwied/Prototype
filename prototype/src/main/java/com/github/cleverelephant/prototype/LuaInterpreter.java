c9e5c29b102dd5305f39908c7daaa29d2226d102
CleverElephant <88872078+CleverElephant@users.noreply.github.com>
2023-12-28 10:09:11 +0100

    fix lua file resolving in jar files
    Signed-off-by: CleverElephant <88872078+CleverElephant@users.noreply.github.com>

--- prototype/src/main/java/com/github/cleverelephant/prototype/LuaInterpreter.java
+++ prototype/src/main/java/com/github/cleverelephant/prototype/LuaInterpreter.java
@@ -1,5 +1,6 @@
 package com.github.cleverelephant.prototype;
 
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -14,11 +15,63 @@ import org.luaj.vm2.LuaString;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
+import org.luaj.vm2.lib.VarArgFunction;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 import org.luaj.vm2.lib.jse.JsePlatform;
 
 public class LuaInterpreter
 {
+    public class CustomSearchPath extends VarArgFunction
+    {
+        public static final String FILE_SEP = "/";
+
+        /* Copied and modified from org.luaj.vm2.lib.PackageLib.searchpath. */
+        @Override
+        public Varargs invoke(Varargs args)
+        {
+            String name = args.checkjstring(1);
+            String path = args.checkjstring(2);
+            String sep = args.optjstring(3, ".");
+            String rep = args.optjstring(4, FILE_SEP);
+
+            // check the path elements
+            int e = -1;
+            int n = path.length();
+            StringBuilder sb = null;
+            name = name.replace(sep.charAt(0), rep.charAt(0));
+            while (e < n) {
+
+                // find next template
+                int b = e + 1;
+                e = path.indexOf(';', b);
+                if (e < 0)
+                    e = path.length();
+                String template = path.substring(b, e);
+
+                // create filename
+                int q = template.indexOf('?');
+                String filename = template;
+                if (q >= 0)
+                    filename = template.substring(0, q) + name + template.substring(q + 1);
+
+                // try opening the file
+                InputStream is = globals.finder.findResource(filename);
+                if (is != null) {
+                    try {
+                        is.close();
+                    } catch (java.io.IOException ioe) {}
+                    return valueOf(filename);
+                }
+
+                // report error
+                if (sb == null)
+                    sb = new StringBuilder();
+                sb.append("\n\t" + filename);
+            }
+            return varargsOf(NIL, valueOf(sb.toString()));
+        }
+    }
+
     private Globals globals;
 
     public LuaInterpreter(Map<String, Object> context)
@@ -35,6 +88,7 @@ public class LuaInterpreter
 
         if (basePath != null)
             globals.get("package").set("path", LuaString.valueOf(basePath.toString()) + "/?.lua");
+        globals.get("package").set("searchpath", new CustomSearchPath());
     }
 
     public void runScript(String file)
